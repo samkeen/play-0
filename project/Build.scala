972a4afb54bd601de72c812b6f793967c94e52a4
samkeen <sam.keen@janrain.com>
2013-09-19 16:03:34 -0700

    init

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "play-todolist"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm,
+    // need this for Heroku
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
